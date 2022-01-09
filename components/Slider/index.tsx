501b4476f53a4b4161d993982b12a3d27181cb6b
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-01-09 19:21:09 -0300

    final

--- components/Slider/index.tsx
+++ components/Slider/index.tsx
@@ -4,41 +4,67 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import Link from 'next/link';
 
 
 export const Slider = () => {
 
-    
+
     return (
-        <Box position={"relative"} marginBlockStart={20}  marginBlockEnd={20} flexDir={"row"} display={"flex"}>
+        <Box position={"relative"} marginBlockStart={20} marginBlockEnd={20} flexDir={"row"} display={"flex"}>
             <Swiper spaceBetween={30} hashNavigation={{
                 "watchState": true
             }} pagination={{
                 "clickable": true
             }} navigation={true}
-            color='color.highlight'
-         
+                color='color.highlight'
+
             >
-                <SwiperSlide  data-hash="slide1"  >
-                    <img src="./continent/europe.png" alt="slide1" width={1700} height={"auto"} />
-                    <Heading size="3xl" position={"absolute"} top={"50%"} left={"50%"} transform={"translate(-50%, -50%)"} color="white" fontWeight="bold" lineHeight={"100px"} textAlign={"center"}>Europa <br /><Heading size="md">O continente mais antigo.</Heading> </Heading>
+                <SwiperSlide data-hash="slide1"  >
+                    <Link href="/continent#europe">
+                        <Box as="a"  >
+                            <img src="./continent/europe.png" alt="slide1" width={1700} height={"auto"} />
+                            <Heading size="3xl" position={"absolute"} top={"50%"} left={"50%"} transform={"translate(-50%, -50%)"} color="white" fontWeight="bold" lineHeight={"100px"} textAlign={"center"}>Europa <br /><Heading size="md">O continente mais antigo.</Heading> </Heading>
+                        </Box>
+                    </Link>
                 </SwiperSlide>
 
                 <SwiperSlide data-hash="slide2">
-                    <img src="./continent/oceania.png" alt="slide2" width={1700} height={"auto"} />
-                    <Heading size="3xl" position={"absolute"} top={"50%"} left={"50%"} transform={"translate(-50%, -50%)"} color="white" fontWeight="bold" lineHeight={"100px"} textAlign={"center"}>Oceania <br /><Heading size="md">Para aventuras selvagens.</Heading> </Heading>
+                    <Link href="/continent#oceania">
+                        <Box as="a"  >
+                            <img src="./continent/oceania.png" alt="slide2" width={1700} height={"auto"} />
+                            <Heading size="3xl" position={"absolute"} top={"50%"} left={"50%"} transform={"translate(-50%, -50%)"} color="white" fontWeight="bold" lineHeight={"100px"} textAlign={"center"}>Oceania <br /><Heading size="md">Para aventuras selvagens.</Heading> </Heading>
+                        </Box>
+                    </Link>
+                </SwiperSlide>
+
+
+                <SwiperSlide data-hash="slide2">
+                    <Link href="/continent#africa">
+                        <Box as="a"  >
+                            <img src="./continent/africa.jpg" alt="slide3" width={1700} height={"auto"} />
+                            <Heading size="3xl" position={"absolute"} top={"50%"} left={"50%"} transform={"translate(-50%, -50%)"} color="white" fontWeight="bold" lineHeight={"100px"} textAlign={"center"}>Africa <br /><Heading size="md">Para grandes descobertas.</Heading> </Heading>
+                        </Box>
+                    </Link>
+
                 </SwiperSlide>
                 <SwiperSlide data-hash="slide2">
-                    <img src="./continent/africa.jpg" alt="slide3" width={1700} height={"auto"} />
-                    <Heading size="3xl" position={"absolute"} top={"50%"} left={"50%"} transform={"translate(-50%, -50%)"} color="white" fontWeight="bold" lineHeight={"100px"} textAlign={"center"}>Africa <br /><Heading size="md">Para grandes descobertas.</Heading> </Heading>
+                    <Link href="/continent#america">
+                        <Box as="a"  >
+                            <img src="./continent/america.jpg" alt="slide4" width={1700} height={"auto"} />
+                            <Heading size="3xl" position={"absolute"} top={"50%"} left={"50%"} transform={"translate(-50%, -50%)"} color="white" fontWeight="bold" lineHeight={"100px"} textAlign={"center"}>America <br /><Heading size="md">Para muita loucura.</Heading> </Heading>
+                        </Box>
+                    </Link>
+
                 </SwiperSlide>
                 <SwiperSlide data-hash="slide2">
-                    <img src="./continent/america.jpg" alt="slide4" width={1700} height={"auto"} />
-                    <Heading size="3xl" position={"absolute"} top={"50%"} left={"50%"} transform={"translate(-50%, -50%)"} color="white" fontWeight="bold" lineHeight={"100px"} textAlign={"center"}>America <br /><Heading size="md">Para muita loucura.</Heading> </Heading>
-                </SwiperSlide>
-                <SwiperSlide data-hash="slide2">
-                    <img src="./continent/asia.jpg" alt="slide5" width={1700} height={"auto"} />
-                    <Heading size="3xl" position={"absolute"} top={"50%"} left={"50%"} transform={"translate(-50%, -50%)"} color="white" fontWeight="bold" lineHeight={"100px"} textAlign={"center"}>Asia <br /><Heading size="md">Para conhecer diferentes culturas.</Heading> </Heading>
+                    <Link href="/continent#asia">
+                        <Box as="a"  >
+                            <img src="./continent/asia.jpg" alt="slide5" width={1700} height={"auto"} />
+                            <Heading size="3xl" position={"absolute"} top={"50%"} left={"50%"} transform={"translate(-50%, -50%)"} color="white" fontWeight="bold" lineHeight={"100px"} textAlign={"center"}>Asia <br /><Heading size="md">Para conhecer diferentes culturas.</Heading> </Heading>
+                        </Box>
+                    </Link>
+
                 </SwiperSlide>
             </Swiper>
         </Box>
--- pages/Continent/index.tsx
+++ pages/Continent/index.tsx
@@ -1,29 +1,260 @@
-import { Box, Flex, Heading, HStack, Image } from "@chakra-ui/react"
+import { Box, Flex, Heading, HStack, Image, Wrap } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
 import { Header } from "../../components/Header"
 
+
+const data = [
+    {
+        nameContinent: 'Europa',
+        description: ' A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo a península ocidental da Eurásia, a Europa geralmente divide-se da Ásia a leste pela divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste',
+        countries: '50',
+        languagues: '60',
+        numCities: '27',
+        banner: './banner/Europe.png',
+        cities: [
+            {
+                name: 'Londres',
+                country: 'Reino Unido',
+                url: './cards/london.png',
+                flag: './cards/london1.png',
+            },
+            {
+                name: 'Roma',
+                country: 'Italia',
+                url: './cards/rome.png',
+                flag: './cards/rome1.png',
+            },
+            {
+                name: 'Amsterdan',
+                country: 'Holanda',
+                url: './cards/amsterdan.png',
+                flag: './cards/amsterdan1.png',
+            },
+            {
+                name: 'Paris',
+                country: 'França',
+                url: './cards/paris.png',
+                flag: './cards/paris1.png',
+            },
+            {
+                name: 'Praga',
+                country: 'Holanda',
+                url: './cards/praga.png',
+                flag: './cards/praga1.png',
+            },
+        ]
+    },
+    {
+        nameContinent: 'Oceania',
+        description: ' A Oceania é o menor continente da Terra, sendo em maior parte composta pela Austrália, principal e mais desenvolvido país da região. A Oceania é um continente localizado a sudeste da Ásia, compreendendo um conjunto de ilhas somado à Austrália, essa última considerada como uma massa continental chamada de “Australásia”.',
+        countries: '14',
+        languagues: '41',
+        numCities: '15',
+        banner: './banner/Oceania.jpg',
+        cities: [
+            {
+                name: 'Londres',
+                country: 'Reino Unido',
+                url: './cards/london.png',
+                flag: './cards/london1.png',
+            },
+            {
+                name: 'Roma',
+                country: 'Italia',
+                url: './cards/rome.png',
+                flag: './cards/rome1.png',
+            },
+            {
+                name: 'Amsterdan',
+                country: 'Holanda',
+                url: './cards/amsterdan.png',
+                flag: './cards/amsterdan1.png',
+            },
+            {
+                name: 'Paris',
+                country: 'França',
+                url: './cards/paris.png',
+                flag: './cards/paris1.png',
+            },
+            {
+                name: 'Praga',
+                country: 'Holanda',
+                url: './cards/praga.png',
+                flag: './cards/praga1.png',
+            },
+        ]
+    },
+    {
+        nameContinent: 'Africa',
+        description: ' África é o continente localizado ao sul da Europa e sudoeste da Ásia, banhado pelo Oceano Atlântico. África é um dos seis continentes do mundo, sendo o terceiro maior em extensão territorial. O território estende-se por mais de 30 milhões de km², ocupando, aproximadamente, 20% da área continental da Terra.',
+        countries: '88',
+        languagues: '201',
+        numCities: '23',
+        banner: './banner/Africa.jpg',
+        cities: [
+            {
+                name: 'Londres',
+                country: 'Reino Unido',
+                url: './cards/london.png',
+                flag: './cards/london1.png',
+            },
+            {
+                name: 'Roma',
+                country: 'Italia',
+                url: './cards/rome.png',
+                flag: './cards/rome1.png',
+            },
+            {
+                name: 'Amsterdan',
+                country: 'Holanda',
+                url: './cards/amsterdan.png',
+                flag: './cards/amsterdan1.png',
+            },
+            {
+                name: 'Paris',
+                country: 'França',
+                url: './cards/paris.png',
+                flag: './cards/paris1.png',
+            },
+            {
+                name: 'Praga',
+                country: 'Holanda',
+                url: './cards/praga.png',
+                flag: './cards/praga1.png',
+            },
+        ]
+    },
+    {
+        nameContinent: 'America',
+        description: ' América é o maior continente em extensão norte–sul localizado no Hemisfério Ocidental e compreende uma área total de 42.189.120 km2. O continente é habitado por cerca de 902.892.047 pessoas, e nele são faladas diversas línguas, como espanhol, inglês, português, francês, neerlandês e línguas nativas.',
+        countries: '102',
+        languagues: '207',
+        numCities: '42',
+        banner: './banner/America.jpg',
+        cities: [
+            {
+                name: 'Londres',
+                country: 'Reino Unido',
+                url: './cards/london.png',
+                flag: './cards/london1.png',
+            },
+            {
+                name: 'Roma',
+                country: 'Italia',
+                url: './cards/rome.png',
+                flag: './cards/rome1.png',
+            },
+            {
+                name: 'Amsterdan',
+                country: 'Holanda',
+                url: './cards/amsterdan.png',
+                flag: './cards/amsterdan1.png',
+            },
+            {
+                name: 'Paris',
+                country: 'França',
+                url: './cards/paris.png',
+                flag: './cards/paris1.png',
+            },
+            {
+                name: 'Praga',
+                country: 'Holanda',
+                url: './cards/praga.png',
+                flag: './cards/praga1.png',
+            },
+        ]
+    },
+    {
+        nameContinent: 'Asia',
+        description: ' A Ásia é o maior continente em extensão territorial do mundo. A Ásia é o maior continente em área terrestre do mundo, estando boa parte do continente localizado no Hemisfério Norte. Além de ser o mais extenso, é também o mais populoso, habitando nele cerca de três quintos da população mundial.',
+        countries: '92',
+        languagues: '303',
+        numCities: '71',
+        banner: './banner/Asia.jpg',
+        cities: [
+            {
+                name: 'Londres',
+                country: 'Reino Unido',
+                url: './cards/london.png',
+                flag: './cards/london1.png',
+            },
+            {
+                name: 'Roma',
+                country: 'Italia',
+                url: './cards/rome.png',
+                flag: './cards/rome1.png',
+            },
+            {
+                name: 'Amsterdan',
+                country: 'Holanda',
+                url: './cards/amsterdan.png',
+                flag: './cards/amsterdan1.png',
+            },
+            {
+                name: 'Paris',
+                country: 'França',
+                url: './cards/paris.png',
+                flag: './cards/paris1.png',
+            },
+            {
+                name: 'Praga',
+                country: 'Holanda',
+                url: './cards/praga.png',
+                flag: './cards/praga1.png',
+            },
+        ]
+    },
+]
+
 export default function continent() {
+
+
+    const [index, setIndex] = useState(null)
+    useEffect(() => {
+        async function getIndex() {
+            let urlData = window.location.hash.split("#")[1]
+            if (urlData == 'europe') {
+                setIndex(0)
+            }
+            if (urlData == 'oceania') {
+                setIndex(1)
+            }
+            if (urlData == 'africa') {
+                setIndex(2)
+            }
+            if (urlData == 'america') {
+                setIndex(3)
+            }
+            if (urlData == 'asia') {
+                setIndex(4)
+            }
+        }
+        getIndex()
+    }, [])
+
+    console.log()
     return (
         <>
             <Box marginBlockEnd={20}>
                 <Header hasBack={true} />
                 <Flex position={"relative"}>
-                    <Image src='./banner/Europe.png' alt='Continent' w="100%" />
+                    <Image src={data[index]?.banner} alt='Continent' w="100%" />
                     <Heading position={"absolute"} size="2xl" color="white" fontWeight="semibold" lineHeight="60px" textAlign="center" mt="20px" left="10%" bottom="10%">
-                        Europa
+                        {data[index]?.nameContinent}
                     </Heading>
                 </Flex>
             </Box>
             <Flex w="100%" my="20" maxW={1480} mx="auto" px="6" marginBlockEnd={20}>
                 <Box w="50%" my="20" mx="auto" px="6">
                     <Heading size="lg" color="color.dark" fontWeight="regular" lineHeight="40px" textAlign="justify" mt="20px" left="10%" bottom="10%">
-                        A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo a península ocidental da Eurásia, a Europa geralmente divide-se da Ásia a leste pela divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste
+                        {data[index]?.description}
                     </Heading>
                 </Box>
                 <Box w="50%" h="auto" justifyContent={"center"} alignSelf={"center"} my="20" mx="auto" ml="20" px="6">
                     <HStack spacing={20}>
                         <Box>
                             <Heading size="3xl" color="color.highlight" fontWeight="semibold" textAlign="center">
-                                50
+                                {data[index]?.countries}
                                 <Heading size="lg" color="color.dark" fontWeight="semibold" textAlign="center">
                                     países
                                 </Heading>
@@ -32,7 +263,7 @@ export default function continent() {
                         </Box>
                         <Box>
                             <Heading size="3xl" color="color.highlight" fontWeight="semibold" textAlign="center">
-                                60
+                                {data[index]?.languagues}
                                 <Heading size="lg" color="color.dark" fontWeight="semibold" textAlign="center">
                                     línguas
                                 </Heading>
@@ -43,7 +274,7 @@ export default function continent() {
                             <Box w="90%">
 
                                 <Heading size="3xl" color="color.highlight" fontWeight="semibold" textAlign="center">
-                                    27
+                                    {data[index]?.numCities}
                                     <Heading size="lg" color="color.dark" fontWeight="semibold" textAlign="center">
                                         cidades +100
                                     </Heading>
@@ -58,44 +289,46 @@ export default function continent() {
                     </HStack>
                 </Box>
             </Flex>
-            <Flex w="100%" my="20" maxW={1480} mx="auto" px="6">
+            <Box w="100%" my="20" maxW={1480} mx="auto" px="6">
                 <Box w="100%" marginBlockEnd={20}>
                     <Heading size="xl" color="color.dark" fontWeight="medium" >
                         Cidades +100
                     </Heading>
                 </Box>
-                <HStack spacing={20}>
-                    <Box w="300px" h="340px">
-                        <Box>
+                <Box w="100%">
+                    <Wrap spacing='75px' >
+                        {
+                            data[index]?.cities.map((item, index) => (
+                                <Box w="300px" h="340px" borderRadius={6} marginBlockEnd={20}>
+                                    <Box w="100%" h="59%">
+                                        <Image src={item.url} alt='Cidade' w="100%" h="auto" />
+                                    </Box>
+                                    <Box borderRadius={6} w="100%" h="40%" borderBlockEnd={"1px solid #FFBA08"} borderLeft={"1px solid #FFBA08"} borderRight={"1px solid #FFBA08"} display={"flex"} >
+                                        <Flex w="100%" mx="auto" px="6" justifyContent={"space-between"} justify={"center"} alignItems={"center"}>
+                                            <Flex>
+                                                <Heading size="lg" color="color.dark" fontWeight="semibold" >
+                                                    {item.name}
+                                                    <Heading size="md" color="color.info" fontWeight="semibold" >
+                                                        {item.country}
 
-                        </Box>
-                        <Box >
+                                                    </Heading>
+                                                </Heading>
+                                            </Flex>
+                                            <Flex>
+                                                <Image src={item.flag} alt='Flag' w="35px" h="auto" />
+                                            </Flex>
+                                        </Flex>
+                                    </Box>
+                                </Box>
+                            ))
+                        }
+                       
 
-                        </Box>
-                    </Box>
-                    <Box w="300px" h="340px" borderRadius={6}>
-                        <Box w="100%" h="59%">
-                            <Image src='./cards/london.png' alt='Cidade' w="100%" h="auto" />
-                        </Box>
-                        <Box borderRadius={6} w="100%" h="40%" borderBlockEnd={"1px solid #FFBA08"} borderLeft={"1px solid #FFBA08"} borderRight={"1px solid #FFBA08"}  display={"flex"} >
-                            <Flex w="100%" mx="auto" px="6" justifyContent={"space-between"} justify={"center"} alignItems={"center"}>
-                                <Flex>
-                                    <Heading size="lg" color="color.dark" fontWeight="semibold" >
-                                        Londres
-                                        <Heading size="md" color="color.info" fontWeight="semibold" >
-                                            Reino Unido
 
-                                        </Heading>
-                                    </Heading>
-                                </Flex>
-                                <Flex>
-                                    <Image src='./cards/london1.png' alt='Flag' w="35px" h="auto" />
-                                </Flex>
-                            </Flex>
-                        </Box>
-                    </Box>
-                </HStack>
-            </Flex>
+                    </Wrap>
+                </Box>
+            </Box>
         </>
     )
 }
+
